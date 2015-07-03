6e07130c9ceb0fd8754693856f80292655bfb1e7
jiangwei <jiangwei@linux-v8fb.site>
2015-07-03 18:17:18 +0800

    loading

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
 
  app.get('/*', function(req, res){
      if(req.useragent.isiPhone||req.useragent.isiPod||req.useragent.isiPad){
-       res.redirect('itms-services://?action=download-manifest&url=https://gengen1988.github.io/cordova-remote-crosswalk/app.plist');
+       res.redirect('itms-services://?action=download-manifest&url=https://jiangwei1995.github.io/cordova-remote-crosswalk/app.plist');
      }else if(req.useragent.isAndroid){
        res.redirect('http://zhushou.360.cn/detail/index/soft_id/3007124')
      }else{
